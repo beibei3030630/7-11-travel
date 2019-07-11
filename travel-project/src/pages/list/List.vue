85953eede1b496fe56eda9c3854de3a7ac0021ba
beibei3030630 <1686310541@qq.com>
2019-07-11 22:38:46 +0800

    change pages

---
+++ travel-project/src/pages/list/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      List
+  </div>
+</template>
+
+<script>
+export default {
+  name:"List",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
